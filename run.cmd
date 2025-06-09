52c4601121623303c19fff79cf96be20a8b9d3bc
zjw939057120 <zjw939057120@outlook.com>
2025-06-09 14:31:12 +0800

    首次提交

---
+++ run.cmd
@@ -0,0 +1,9 @@
+cd cmake-build-debug
+cmake --build . --target all -- -j 64
+
+adb root
+adb shell killall -9 serial
+adb push TcpServer /data/local
+adb shell chmod -R 755  /data/local/*
+adb shell /data/local/TcpServer
+
